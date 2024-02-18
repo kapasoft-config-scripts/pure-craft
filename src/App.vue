f89c79ec979ba3602b6d2c9ade2d2c87af24c15c
Margots Kapačs <mkapacs@gmail.com>
2024-02-18 18:33:04 +0200

    add first template - default home

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomeDefault />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeDefault from './components/HomePages/HomeDefault.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeDefault
   }
 }
 </script>
